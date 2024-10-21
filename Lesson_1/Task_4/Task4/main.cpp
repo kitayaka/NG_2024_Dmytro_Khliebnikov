c1ba0b4c336916ca09f0776be2fef7705e2b0c14
Dmytro Khliebnikov <hlebnikovdmitro503@gmail.com>
2024-10-21 22:20:01 +0300

    C++

--- Lesson_1/Task_4/Task4/main.cpp
+++ Lesson_1/Task_4/Task4/main.cpp
@@ -5,15 +5,18 @@ using namespace std;
 int main()
 {
     int money;
+    cout << "What's your salary?" << endl;
     cin >> money;
-    if (money < 1000){
-        cout << "Rabotai bolshe" <<endl;
+
+    if (money < 1000) {
+        cout << "work harder" << endl;
     }
-    if (money > 1000){
-        if (money <1000000)
-            cout << "Molodes"<<endl;
+    if (money > 1000) {
+        if (money < 1000000) {
+            cout << "well done!" << endl;
+        }
     }
-    if(money > 1000000 ){
-        cout <<"Milloener"<<endl;
+    if (money > 1000000) {
+        cout << "millionaire" << endl;
     }
 }
---
+++ Lesson_2/Task1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char count[5];
+    cout << "lol" <<endl;
+    cin >> count;
+    cout << " " << count[5];
+
+}
