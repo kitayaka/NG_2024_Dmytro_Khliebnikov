1fd91902f366207137f151e5f21dcb89651aa90f
Dmytro Khliebnikov <hlebnikovdmitro503@gmail.com>
2024-11-07 17:10:09 +0200

    Lecture 2 task 1 was made to lead 5 numbers so they were by comma

--- Lesson_2/Task1/main.cpp
+++ Lesson_2/Task1/main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-    char count[5];
-    cout << "lol" <<endl;
-    cin >> count;
-    cout << " " << count[5];
-
+    int massive[5];
+    cout << "Enter five numbers: ";
+    for (int number = 0; number < 5; number++)
+    {
+    cin >> massive[number];
+    cout << massive[number];
+    if (number < 4)
+    cout << ",";
+    }
 }
